abd778dd56379d4328795259f3c695415569699c
NIHARIKA-SUREDDI <147476965+NIHARIKA-SUREDDI@users.noreply.github.com>
2023-10-10 12:39:03 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Great from './components/great';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome Back</h1>
+      <Great/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/great.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Great() {
+    return (
+        <h1>Niharika</h1>
+    )
+}
+
+// export default Great;
